76fc831c313bf0968b267d9ee3c32b6f713534ed
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-03-17 19:46:07 +0100

    Removed optimal step size from diff plot

--- HW_2/diff_approximation_plot.py
+++ HW_2/diff_approximation_plot.py
@@ -31,8 +31,6 @@ def plot(min_n, max_n, epsilon):
     plt.plot(n_values, truncation_errs, label="Truncation error")
     plt.plot(n_values, round_off_errs, label="Roundoff error")
     plt.plot(n_values, tot_errs, label="Total error")
-    h_opt = np.log10(np.sqrt((4 * epsilon) / np.sin(1)))
-    plt.axvline(h_opt, label="Optimal step size", color="red")
     plt.yscale('log')
     plt.legend()
     plt.xlabel("x")

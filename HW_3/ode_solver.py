a817dff1badf0891fbdce882abaab7c4f3ffed74
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-03-31 21:02:43 +0200

    Update ode_solver.py

--- HW_3/ode_solver.py
+++ HW_3/ode_solver.py
@@ -5,7 +5,6 @@ class ODESolver():
     def newtons_method(f, f_prime, x_k, tol):
         while np.abs(f(x_k)) > tol:
             x_k = x_k - f(x_k) / f_prime(x_k)
-        print(f(x_k))
         return x_k
 
     @staticmethod
@@ -18,17 +17,29 @@ class ODESolver():
                 b = m
         print(f(m))
         return m
-        
+
 
     @staticmethod
     def forward_euler(derivative_function, delta_t, t_n, w_n, derivative_function_prime):
         return w_n + delta_t * derivative_function(t_n, w_n)
-    
+
     def backward_euler(derivative_function, delta_t, t_n, w_n, derivative_function_prime):
-        f = lambda x: -x + w_n + delta_t * derivative_function(t_n + delta_t, x)
-        f_prime = lambda x: -1 + delta_t * derivative_function_prime(t_n + delta_t, x)
-        return ODESolver.newtons_method(f, f_prime, w_n, 10**-10)
-    
+        if delta_t < 3:
+            f = lambda x: -x + w_n + delta_t * derivative_function(t_n + delta_t, x)
+            f_prime = lambda x: -1 + delta_t * derivative_function_prime(t_n + delta_t, x)
+            print(w_n)
+            return ODESolver.newtons_method(f, f_prime, w_n, 10-8)
+        else:
+            k = 5 * 10-6
+            m = 105
+            a = delta_t * k
+            b = -1
+            c = -(w_n + delta_t * k * m)
+            numerator = -b + np.sqrt(b2 - 4 * a * c)
+            denominator = 2 * a
+            return numerator/denominator
+
+
     @staticmethod
     def solve_initial_value_problem(derivative_function, delta_t, time_integration_method, initial_value, n_steps, derivative_function_prime = None):
         w_values = []
@@ -43,6 +54,5 @@ class ODESolver():
             w_i = time_integration_method(derivative_function, delta_t, t_i, w_i, derivative_function_prime)
             w_values.append(w_i)
             t_values.append(t_i)
-        
+
         return t_values, w_values
-            
